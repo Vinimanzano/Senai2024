988188fc9f5209ebdb8871c2452452a62e60ef0f
vinimanzano <vini.manzano5@gmail.com>
2024-08-06 10:41:44 -0300

    senai

---
+++ 01-Projects/ProjectJWT/front/js/index.js
@@ -0,0 +1,17 @@
+const TIMEOUT_DURATION = 1 * 60 * 1000;
+
+let timeout;
+
+function resetTimeout() {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+        window.location.href = './html/login.html';
+    }, TIMEOUT_DURATION);
+}
+
+window.onload = resetTimeout;
+document.onmousemove = resetTimeout;
+document.onkeypress = resetTimeout;
+document.onclick = resetTimeout;
+document.onscroll = resetTimeout;
+
