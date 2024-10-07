bd17186aa8bce1b7f84258630081ded928097772
vinimanzano <vini.manzano5@gmail.com>
2024-10-07 15:48:15 -0300

    update

---
+++ 01-Projects/Aula 07 - 25-09-24/aula07/aula07/src/main/java/senai/aula07/cors/CorsConfig.java
@@ -0,0 +1,21 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Permitir todas as rotas
+                        .allowedOrigins("*") // Permitir todas as origens
+                        .allowedMethods("*") // Permitir todos os métodos (GET, POST, PUT, DELETE, etc.)
+                        .allowedHeaders("*"); // Permitir todos os cabeçalhos
+            }
+        };
+    }
+}
