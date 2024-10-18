163acd03df69d9ed4c6badbcfd7ce0812c9d1af2
vinimanzano <vini.manzano5@gmail.com>
2024-10-18 10:55:35 -0300

    SENAI

--- 01-Projects/Aula 09 - 07-10-24/Front/JS/novoequipamento.js
+++ 01-Projects/Aula 09 - 07-10-24/Front/JS/novoequipamento.js
@@ -1,4 +1,3 @@
-// Função para atualizar o status do checkbox
 function updateStatus() {
     const checkbox = document.getElementById('checkbox');
     const statusText = document.getElementById('status');
@@ -10,18 +9,14 @@ function updateStatus() {
     }
 }
 
-// Adiciona o evento de mudança ao checkbox
 document.getElementById('checkbox').addEventListener('change', updateStatus);
 
-// Adiciona o evento de envio ao formulário
 const form = document.querySelector('.form');
 form.addEventListener('submit', async (e) => {
     e.preventDefault();
 
-    // Verifica o status do checkbox
     const status = form.checkbox.checked ? 'ativo' : 'desativo';
 
-    // Cria o objeto equipamento com os dados do formulário
     const equipamento = {
         nome: form.nome.value,
         imagem: form.imagem.value,
@@ -32,7 +27,6 @@ form.addEventListener('submit', async (e) => {
     console.log(equipamento);
 
     try {
-        // Envia os dados para o servidor
         const response = await fetch('http://localhost:3000/equipamentos', {
             method: 'POST',
             headers: {
@@ -41,15 +35,12 @@ form.addEventListener('submit', async (e) => {
             body: JSON.stringify(equipamento)
         });
 
-        // Verifica se a resposta é OK
         if (!response.ok) {
             throw new Error(`Erro: ${response.status} - ${response.statusText}`);
         }
 
-        // Aguarda a resposta em JSON
         const data = await response.json();
         
-        // Redireciona para a página de equipamentos
         window.location.href = 'equipamentos.html';
         console.log(data);
     } catch (error) {
