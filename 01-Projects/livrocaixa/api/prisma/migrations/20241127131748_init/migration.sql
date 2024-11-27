d318d5d3ef58f748daf2f5288c7d1e2d246a4935
vinimanzano <vini.manzano5@gmail.com>
2024-11-27 10:21:18 -0300

    SENAI

--- 01-Projects/livrocaixa/api/prisma/migrations/20241127131748_init/migration.sql
+++ 01-Projects/livrocaixa/api/prisma/migrations/20241127131748_init/migration.sql
@@ -1,19 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `tb_lancamentos` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `tb_usuario` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropForeignKey
-ALTER TABLE `tb_lancamentos` DROP FOREIGN KEY `Tb_lancamentos_usuario_id_fkey`;
-
--- DropTable
-DROP TABLE `tb_lancamentos`;
-
--- DropTable
-DROP TABLE `tb_usuario`;
-
 -- CreateTable
 CREATE TABLE `Usuario` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
--- 01-Projects/livrocaixa/api/src/controllers/usuarios.js
+++ 01-Projects/livrocaixa/api/src/controllers/usuarios.js
@@ -1,19 +1,19 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 const create = async (req, res) => {
-    const usuario = await prisma.usuario.create({
-        data: req.body
-    });
-    res.status(201).json(usuario);
-}
+  const usuario = await prisma.usuario.create({
+    data: req.body,
+  });
+  res.status(201).json(usuario);
+};
 
 const read = async (req, res) => {
-    const usuarios = await prisma.usuario.findMany();
-    res.json(usuarios);
-}
+  const usuarios = await prisma.usuario.findMany();
+  res.json(usuarios);
+};
 
-module.exports = { 
-    create, 
-    read 
+module.exports = {
+  create,
+  read,
 };
